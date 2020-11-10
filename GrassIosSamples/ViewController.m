7c9f50d0d67ae9a997551a95c054552e46e1a9cb
grass <grassswwang@tencent.com>
2020-11-10 12:20:11 +0800

    添加UITableView的测试用例

--- GrassIosSamples/ViewController.m
+++ GrassIosSamples/ViewController.m
@@ -7,7 +7,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource>
+
+@property(nonnull,nonatomic,strong) UITableView *myTableView;
 
 @end
 
@@ -15,7 +17,39 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    NSLog(@"viewDidLoad is working");
     // Do any additional setup after loading the view.
+//    [self.view addSubview:[self myTableView]];
+    [self.view addSubview:self.myTableView];
+}
+
+- (UITableView *)myTableView {
+    NSLog(@"myTabView create is working1: %@",_myTableView);
+    if (_myTableView == nil) {
+        _myTableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+        _myTableView.backgroundColor = UIColor.blueColor;
+        _myTableView.dataSource = self;
+        NSLog(@"myTabView create is working");
+    }
+    return _myTableView;
+}
+
+
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    static NSString *cellid = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellid];
+    if(cell == nil){
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellid];
+        NSString * text = [NSString stringWithFormat:@"%li",(long)indexPath.row];
+        cell.textLabel.text = text;
+        NSLog(@"UITableViewCell create is working %@",text);
+    }
+    return cell;
+}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 10;
 }
 
 
