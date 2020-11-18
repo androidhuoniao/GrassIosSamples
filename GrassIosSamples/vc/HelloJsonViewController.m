aae8d810c27a7daecd10f462807c25db396b1938
grasss <androidhuoniao@hotmail.com>
2020-11-18 20:33:00 +0800

    添加Json的解析测试用例，同时学习了NSDictionary的valueForKeyPath用法

--- GrassIosSamples/vc/HelloJsonViewController.m
+++ GrassIosSamples/vc/HelloJsonViewController.m
@@ -2,6 +2,8 @@
 //  HelloJsonViewController.m
 //  GrassIosSamples
 //  Json解析的各种测试用例
+//  NSDictionary的keypath用法真是牛逼
+//  [NSDictionary \| Apple Developer Documentation](https://developer.apple.com/documentation/foundation/nsdictionary?language=objc)
 //  Created by grassswwang(王圣伟) on 2020/11/18.
 //
 
@@ -15,17 +17,42 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+   
+    NSString *jsonStr = [self obj2Json];
+    NSDictionary *dict = [self json2Obj:jsonStr];
+    NSLog(@"--------------dict----------\n:%@",dict);
+    NSString *carname = [dict valueForKeyPath:@"car.carname"];
+    NSLog(@"carname=%@",carname);
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(NSDictionary *) json2Obj:(NSString*) jsonStr{
+    NSData *jsonData = [jsonStr dataUsingEncoding:NSUTF8StringEncoding];
+    NSError *err;
+    NSDictionary *dic = [NSJSONSerialization JSONObjectWithData:jsonData
+                                                        options:NSJSONReadingMutableContainers
+                                                          error:&err];
+    if(err) {
+        NSLog(@"json解析失败：%@",err);
+    }else{
+        NSLog(@"json解析成功：%@",dic);
+    }
+    return dic;
 }
-*/
 
+-(NSString*) obj2Json{
+
+    NSDictionary *cardict = @{@"carname":@"audi",@"price":@100000};
+        
+    NSMutableDictionary *personDict = [NSMutableDictionary dictionary];
+    [personDict setValue:@"grass" forKey:@"name"];
+    [personDict setValue:@32 forKey:@"age"];
+    [personDict setValue:cardict forKey:@"car"];
+    NSLog(@"personDict:%@",personDict);
+    
+    NSError *error = nil;
+    NSData *data = [NSJSONSerialization dataWithJSONObject:personDict options:NSJSONWritingPrettyPrinted error:&error];
+    NSString *jsonString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    NSLog(@"jsonString %@", jsonString);
+    return jsonString;
+}
 @end
