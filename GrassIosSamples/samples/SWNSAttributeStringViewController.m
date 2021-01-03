b78e926e4bf8ad38f885780a7ea2aef234005547
grassswwang <grassswwang@tenceng.com>
2021-01-03 20:12:39 +0800

    添加计算文字高度的函数

--- GrassIosSamples/samples/SWNSAttributeStringViewController.m
+++ GrassIosSamples/samples/SWNSAttributeStringViewController.m
@@ -16,11 +16,36 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self.view addSubview:self.uilabel];
-    [self setAttributeString];
+    self.uilabel.attributedText = [self getAttributeString];
+    [self testsizeThatFits];
+    [self testBoundingRectSize];
 }
 
-- (void) setAttributeString{
-    NSMutableAttributedString *vipStr = [[NSMutableAttributedString alloc]initWithString:@"点击解锁【超级方法"];
+- (void) testsizeThatFits{
+    UILabel *label = [[UILabel alloc] init];
+    label.numberOfLines = 2;
+    label.font = [UIFont systemFontOfSize:20];
+    label.attributedText = [self getAttributeString];
+    CGSize size = [label sizeThatFits:CGSizeMake(self.view.bounds.size.width, 100)];
+    NSLog(@"testsizeThatFits size:%@",NSStringFromCGSize(size));
+}
+
+/**
+ 这种方式计算出来的大小是整个文本的大小，虽然设置了constraintSize，但是仅仅是个参考，例如下面的代码，最终的返回值为
+ {387.21999999999997, 144.16015625}，高度比100大得多，并且不受行数限制
+ options必需写为options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading，否则计算的高度不准确
+ */
+- (void)testBoundingRectSize{
+    NSAttributedString *attributeStr =  [self getAttributeString];
+    CGRect rect = [attributeStr boundingRectWithSize:CGSizeMake(self.view.bounds.size.width, 100)
+                                             options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading
+                                             context:nil];
+//    self.uilabel.frame = CGRectMake(0, 100, self.view.bounds.size.width,rect.size.height);
+    NSLog(@"testsizeThatFits rect:%@",NSStringFromCGSize(rect.size));
+}
+
+- (NSAttributedString *) getAttributeString{
+    NSMutableAttributedString *vipStr = [[NSMutableAttributedString alloc]initWithString:@"点击解锁【超级方法点击解锁【超级方法点击解锁【超级方法点击解锁【超级方法点击解锁【超级方法点击解锁【超级方法点击解锁【超级方法点击解锁【超级方法点击解锁【超级方法点击解锁【超级方法点击解锁【超级方法点击解锁【超级方法"];
     UIImage *vipImage = [UIImage imageNamed:@"vscode_icon"];
     NSTextAttachment *vipImageAttachment = [[NSTextAttachment alloc]init];
     vipImageAttachment.image = vipImage;
@@ -35,16 +60,18 @@
     [vipStr addAttribute:NSKernAttributeName value:@(6) range:NSMakeRange(1,1)];
     //设置字体和设置字体的范围
     [vipStr addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:20.0f] range:NSMakeRange(0, vipStr.length)];
-    self.uilabel.attributedText = vipStr;
+    return vipStr;
 }
 
 - (UILabel *)uilabel{
     if (!_uilabel) {
         UILabel *label = [[UILabel alloc] init];
         label.backgroundColor = [UIColor orangeColor];
+        label.numberOfLines = 2;
         label.frame = CGRectMake(0, 100, self.view.bounds.size.width,100);
         _uilabel = label;
     }
     return _uilabel;
 }
+
 @end
