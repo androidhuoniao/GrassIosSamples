594c952429e5efcb758c96909c7f17877d67cf3c
grassswwang <grassswwang@tencent.com>
2021-01-04 16:26:11 +0800

    删除无用的代码

--- GrassIosSamples/samples/SWUILableViewController.m
+++ GrassIosSamples/samples/SWUILableViewController.m
@@ -16,7 +16,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-//    [self addHtmlUILable];
     [self addRichText2];
 }
 
@@ -31,37 +30,6 @@
     [self.view addSubview:myLabel];
 }
 
--(void) addRichText{
-    NSString *imageName = @"lable_icon";
-    NSString *title = @"afdadlkfjalkadsfkaldjfklajlkdjfklajdlkfjakldsjfklajlkjflkadjlkfkasjdflkajkldjaklfjakldjflkajl;fdjaslfkjaklfjkadljflka";
-    
-    //设置Vip标志
-    NSTextAttachment *imageAttachment = [[NSTextAttachment alloc] init];
-    //设置目标图片
-    imageAttachment.image = [UIImage imageNamed:imageName];
-
-    //设置大小和位置
-    imageAttachment.bounds = CGRectMake(3, -10, imageAttachment.image.size.width, imageAttachment.image.size.height);
-    //初始化富文本字符串 并携带NSTextAttachment
-    NSAttributedString *attachmentString = [NSAttributedString attributedStringWithAttachment:imageAttachment];
-    //初始化一个空的可变富文本字符串
-    NSMutableAttributedString *completeText= [[NSMutableAttributedString alloc] initWithString:@""];
-    //拼接之前的attachmentString字符串
-    [completeText appendAttributedString:attachmentString];
-    //初始化目标富文本字符串
-    NSMutableAttributedString *textAfterIcon= [[NSMutableAttributedString alloc] initWithString:title];
-    //拼接目标富文本内容
-    [completeText appendAttributedString:textAfterIcon];
-    
-    /*备注：上边这样的顺序执行下来是先小图标在文本内容如上图一样，如果你图标想放在后面，就无需多拼接一次，直接用目标文本去拼接图标就可以，[completeText appendAttributedString:attachmentString];*/
-    UILabel *myLabel = [[UILabel alloc] initWithFrame:self.view.bounds];
-    myLabel.frame = CGRectMake(5,200, 350,100);
-    myLabel.backgroundColor = UIColor.whiteColor;
-    myLabel.numberOfLines = 1;
-    myLabel.font = [UIFont fontWithName:@"" size:16];
-    [myLabel setAttributedText:completeText];
-    [self.view addSubview:myLabel];
-}
 
 -(void) addRichText2{
     NSString *title = @"上边这样的顺序执行下来是先小图标在文本内容如上图一样，如果你图标想放在后面，就无需多拼接一次，直接用目标文本去拼接图标就可以";
